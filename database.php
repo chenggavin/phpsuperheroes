387116370df8efeaff6d4f2f207f370801022b88
Gavin Cheng <gavincheng01@gmail.com>
2017-10-27 18:15:00 -0400

    fixed database

--- database.php
+++ database.php
@@ -6,9 +6,9 @@
 
 
 <?php
-  function getDb() {
-    $db = pg_connect("host=localhost port=5432 dbname=super_dev user=superuser password=supersupersuper");
-    return $db;
+  // function getDb() {
+  //   $db = pg_connect("host=localhost port=5432 dbname=super_dev user=superuser password=supersupersuper");
+  //   return $db;
   }
 ?>
 
@@ -21,24 +21,24 @@
 <!-- 						FOR HEROKU PUSH! 						-->
 
 <?php
-  // function getDb() {
-  //   if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/.env')) {
-  //     require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
-  //     $dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
-  //     $dotenv->load();
-  //   }
-  //   $url = parse_url(getenv("DATABASE_URL"));
-  //   $db_port = $url['port'];
-  //   $db_host = $url['host'];
-  //   $db_user = $url['user'];
-  //   $db_pass = $url['pass'];
-  //   $db_name = substr($url['path'], 1);
-  //   $db = pg_connect(
-  //     "host=" . $db_host .
-  //     " port=" . $db_port .
-  //     " dbname=" . $db_name .
-  //     " user=" . $db_user .
-  //     " password=" . $db_pass);
-  //   return $db;
-  // }
+  function getDb() {
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/.env')) {
+      require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+      $dotenv = new Dotenv\Dotenv($_SERVER['DOCUMENT_ROOT']);
+      $dotenv->load();
+    }
+    $url = parse_url(getenv("DATABASE_URL"));
+    $db_port = $url['port'];
+    $db_host = $url['host'];
+    $db_user = $url['user'];
+    $db_pass = $url['pass'];
+    $db_name = substr($url['path'], 1);
+    $db = pg_connect(
+      "host=" . $db_host .
+      " port=" . $db_port .
+      " dbname=" . $db_name .
+      " user=" . $db_user .
+      " password=" . $db_pass);
+    return $db;
+  }
 ?>
